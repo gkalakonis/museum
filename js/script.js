952e759ab0bb75bd9ab15dd08f68d1805b27d3c0
gkalakonis <up1083846@ac.upatras.gr>
2024-03-24 15:30:01 +0200

    newsbar

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('.news-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+        prevArrow: '', // Set to empty string to disable the left arrow
+        nextArrow: '<button type="button" class="slick-next">Next</button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
